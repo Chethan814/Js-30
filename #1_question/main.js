86934dbe494aa7ae8e31ab40ed6bbaedcb84a0bf
chethan <chethanchethu99339@gmail.com>
2024-12-23 21:45:33 +0530

    leetcode js 30 first question and solution , along with function closures and lexical scoping

---
+++ #1_question/main.js
@@ -0,0 +1,13 @@
+function createHelloword(){
+    return ()=> "Hello World"
+}
+
+let f = createHelloword
+
+f()
+
+// * i dont get it 
+// * In brower this code is excuted but not in node enveronment 
+
+// ? what copilot give to me 
+
