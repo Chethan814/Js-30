c4df95c1444411a0dc6645e04ded85c35524aa39
chethan <chethanchethu99339@gmail.com>
2025-01-04 19:08:01 +0530

    fix array manipulation logic and add map example for doubling values

--- #5_question/array.js
+++ #5_question/array.js
@@ -3,10 +3,16 @@ let newarr = []
 
 for (let i = 0; i < arr.length; i++) {
     
-    newarr = arr[i] * 2;
-    return newarr
+    newarr[i] = arr[i] * 2;
     
 }
 
 console.log(newarr);
 
+let maparray = [2, 10 , 20 , 30]
+
+let doubleArray = maparray.map((value,index,arr)=>{
+    return value * 2   
+})
+
+console.log(doubleArray);
